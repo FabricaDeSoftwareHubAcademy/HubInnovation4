fbdc9f869c467ad6a85f450224675e345bbb771c
thiagocghc <50628917+thiagocghc@users.noreply.github.com>
2024-05-19 13:52:41 -0300

    cadastro de ambientes, palestras e palestrantes

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if(isset($_POST['logar'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HUB INNOVATION II</title>
+    <title>HUB INNOVATION</title>
     <link rel="stylesheet" href="./CSS/menu.css">
     <link rel="stylesheet" href="./CSS/login.css">
 </head>
